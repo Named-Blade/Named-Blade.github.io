e82020161f8a10263fe35a8dabd67e7f45652695
named-blade <named.blade@gmail.com>
2023-03-10 05:55:29 +0000

    partial fix for old browsers, also renaming.

--- js/wheelmenu.js
+++ js/wheelmenu.js
@@ -1,13 +1,3 @@
-for (styleSheet of document.styleSheets){
-	if (/wheelmenu\.css$/.test(styleSheet.href)){
-		for (let i=0;i<styleSheet.cssRules.length;i++){
-			if (/menu-item:nth-child/.test(styleSheet.cssRules[i].selectorText)){;
-				styleSheet.deleteRule(i);
-				i--
-			}
-		}
-	}
-}
 window.addEventListener('load', function () {
 	let i=0
 	for (nav of document.getElementsByClassName('menu')){
@@ -15,14 +5,24 @@ window.addEventListener('load', function () {
 		new navMenu(nav,navClass);
 		i++;
 	}
+	for (styleSheet of document.styleSheets){
+		if (/wheelmenu\.css$/.test(styleSheet.href)){
+			for (let i=0;i<styleSheet.cssRules.length;i++){
+				if (/menu-item:nth-child/.test(styleSheet.cssRules[i].selectorText)){;
+					styleSheet.deleteRule(i);
+					i--
+				}
+			}
+		}
+	}
 });
 class navMenu{
 	constructor(nav,navClass,n=6){
 		this.nav = nav;
 		this.nav.navMenu = this;
-		this.navclass = navClass;
+		this.navClass = navClass;
 		this.n = n;
-		this.nav.classList.add(this.navclass);
+		this.nav.classList.add(this.navClass);
 		this.observer = new MutationObserver(this.onChange.bind(this));
 		this.observer.observe(this.nav.children[2],{childList:true});
 		this.onChange();
@@ -34,9 +34,8 @@ class navMenu{
 				styleSheets = styleSheets.slice(0,i).concat(styleSheets.slice(i+1))
 			}
 		}
-		document.adoptedStyleSheets = styleSheets;
 		this.styleSheet = new CSSStyleSheet();
-		document.adoptedStyleSheets.push(this.styleSheet)
+		document.adoptedStyleSheets = styleSheets;
 		this.styleSheet.insertRule(`@keyframes spin-1 {
 			from {
 				rotate:0deg;
@@ -61,32 +60,27 @@ class navMenu{
 			let lastCircle = Math.floor(-(1/2)+(n+1+8*(numItems-1))**(1/2)/(2*(n)**(1/2)))+1;
 			let maxSize = n*lastCircle*(lastCircle+1)/2;
 			let circleSize = n*circle;
-			let circlePoints;
-			if (circle == lastCircle && maxSize >= circleSize){
-				circlePoints = numItems-(maxSize-circleSize);
-			} else {
-				circlePoints = circleSize;
-			}
+			let circlePoints = (circle == lastCircle && maxSize >= circleSize)?numItems-(maxSize-circleSize):circleSize;
 			let point = (tau/circlePoints)*(i%circlePoints);
 			let x= Math.cos(point);
 			let y= Math.sin(point);
-			let selector = "."+this.navclass+" .menu-toggler:checked ~ ul .menu-item:nth-child("+(i+1)+")";
+			let selector = "."+this.navClass+" .menu-toggler:checked ~ ul .menu-item:nth-child("+(i+1)+")";
 			let rule = `{
 				animation: spin-`+(circle%2 == 1 ? 1:2)+` 30s linear infinite;
 				transform: rotate(-90deg) translate(`+(x*100*circle)+`%,`+(y*100*circle)+`%);
 			}`;
 			this.styleSheet.insertRule(selector+rule);
-			selector = "."+this.navclass+" .menu-toggler:checked ~ ul .menu-item:nth-child("+(i+1)+") a";
+			selector = "."+this.navClass+" .menu-toggler:checked ~ ul .menu-item:nth-child("+(i+1)+") a";
 			rule = `{
 				animation: spin-`+(circle%2 == 1 ? 2:1)+` 30s linear infinite;
 				transform: rotate(90deg);
 			}`;
 			this.styleSheet.insertRule(selector+rule);
 		}
-		this.nav.classList.remove(this.navclass);
+		this.nav.classList.remove(this.navClass);
 		this.nav.offsetWidth;
-		this.nav.classList.add(this.navclass);
-		
+		this.nav.classList.add(this.navClass);
+		document.adoptedStyleSheets.push(this.styleSheet);
 	}
 	changeN(n){
 		this.n = n;
